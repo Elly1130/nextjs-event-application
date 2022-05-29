b8763a0be7c36df7d0ad918bf6fad14b8965ad83
Elly1130 <xychong-wp20@student.tarc.edu.my>
2022-05-30 03:59:28 +0800

    Apply createGlobalStyle

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import Head from 'next/head';
 
+import GlobalStyle from '../components/GlobalStyle';
 import Layout from '../components/layout/layout';
-import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <GlobalStyle />
       <Head>
         <title>Next Events</title>
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Open+Sans:wght@400;700&display=swap'
+        ></link>
         <meta name='description' content='NextJS Events' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
